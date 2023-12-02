d69490338e949e398f99c380b70c784965ab72c9
Daniel Vartanov <dan@vartanov.net>
2023-12-02 11:04:34 +0000

    Add a test for filled field graphics

--- graphics_test.go
+++ graphics_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestFieldWithPiece(t *testing.T) {
+func TestEmptyFieldWithPiece(t *testing.T) {
 	ts := NewTetris(4, 6)
 	pc := Pieces['I']
 	ts.AddPiece(&pc)
@@ -17,6 +17,37 @@ func TestFieldWithPiece(t *testing.T) {
 		"<! .[][][][] .!>" + "\n\r" +
 		"<! . . . . . .!>" + "\n\r" +
 		"<! . . . . . .!>" + "\n\r" +
+		"<!============!>" + "\n\r" +
+		`  \/\/\/\/\/\/  ` + "\n\r" +
+		"\x1b[0m"
+
+	got := scr.Printable()
+	if got != want {
+		t.Errorf("\n\rGot:\n\r%s\n\rWant:\n\r%s", got, want)
+	}
+}
+
+func TestFilledFieldWithPiece(t* testing.T) {
+	ts := NewTetris(5, 6)
+	pc := Pieces['L']
+	ts.AddPiece(&pc)
+
+	filled := []Coords{{4, 0}, {4, 1}, {3, 0}, {3, 1}, {4, 4}, {4, 5}}
+	for _, coords := range(filled) {
+		ts.field[coords.Row][coords.Col] = true
+	}
+
+	scr := NewScreen(7, 16)
+
+	fld := NewField(&ts, &scr)
+	fld.Render()
+
+	want := "\x1b[32m" +
+        	"<! . . . . . .!>" + "\n\r" +
+		"<! . . .[] . .!>" + "\n\r" +
+		"<! .[][][] . .!>" + "\n\r" +
+		"<![][] . . . .!>" + "\n\r" +
+	        "<![][] . .[][]!>" + "\n\r" +
 		"<!============!>" + "\n\r" +
 		`  \/\/\/\/\/\/  ` + "\n\r" +
 		"\x1b[0m"
